c66a5e8a89405e32e539b667c73cea0205340b15
shenqiz1995 <shenqiz@andrew.cmu.edu>
2019-09-03 22:18:00 -0400

    Add main.py.

---
+++ main.py
@@ -0,0 +1,17 @@
+from simulator.simulator import Simulator 
+from simulator.route_manager import RouteManager
+# from algorithm import AlgorithmManager as algo
+from algorithm import DummyAlgorithmManager as algo
+from connection import ConnectionManager as conn
+rm = RouteManager("simulator/maps/simple")
+#algo = AlgorithmManager
+sim = Simulator(
+    rm,  #route manager
+    algo, #algorithm module
+    conn, #connection module
+    "simulator/maps/simple" #map folder
+    )
+
+
+rm.bind_simulator(sim)
+sim.start_simulation()
